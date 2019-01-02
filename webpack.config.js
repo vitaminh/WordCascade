955b8ffad72e91bc8c51c69a1c37da149f453eeb
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-01 22:58:38 -0800

    Install webpacak-dev-server, display "Hello World" in browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+const path = require('path');
+
 module.exports = {
   entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
   mode: 'development',
   output: {
-    path: __dirname + '/public', // assumes your bundle.js will also be in the root of your project folder
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   devtool: 'source-maps',
+  devServer: {
+    contentBase: './public'
+  },
   module: {
     rules: [
       {
