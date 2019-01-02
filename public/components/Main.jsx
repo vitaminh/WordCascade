742d1d06c8656a4ab67ffc36519a35d2677fe59b
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-02 13:09:45 -0800

    Import React, Create Main Component

---
+++ public/components/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import wordlist from 'wordlist-english';
+
+export default class Main extends React.Component {
+  render() {
+    return <div>Words</div>;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
+  entry: './public/index.js',
   mode: 'development',
   output: {
     path: path.join(__dirname, 'public'),
@@ -19,7 +19,20 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.wasm', '.json']
+  },
+  externals: {
+    fs: "require('fs')"
   }
 };
