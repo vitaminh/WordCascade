5e8b63078f5c764c71e977ca51ae671dbb6f290f
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-03 22:27:19 -0800

    Implement motion of word

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -11,7 +11,7 @@ export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wordlist: [],
+      wordlist: []
     };
   }
 
@@ -21,7 +21,7 @@ export default class Main extends React.Component {
     this.setState(prevState => {
       return {
         ...prevState,
-        wordlist,
+        wordlist
       };
     });
   }
@@ -29,9 +29,7 @@ export default class Main extends React.Component {
   render() {
     const myCanvas = document.getElementById('paper-canvas');
     paper.setup(myCanvas);
-    let width = paper.view.size.width;
-    let height = paper.view.size.height;
-    const text = new paper.PointText(new Point(width / 2, height / 2));
+    const text = new paper.PointText(new Point(paper.view.center));
     text.justification = 'center';
     text.fontSize = '1.2em';
     const randomIndex = getRandomInt(0, this.state.wordlist.length);
@@ -39,15 +37,32 @@ export default class Main extends React.Component {
 
     // render
     paper.view.draw();
+    let destination = Point.random().multiply(paper.view.size);
     paper.view.onFrame = function(event) {
-      text.rotate(0.5);
+      // Each frame, move the path 1/30th of the difference in position
+      // between it and the destination.
+
+      // The vector is the difference between the position of
+      // the text item and the destination point:
+      const vector = destination.subtract(text.position);
+
+      // We add 1/30th of the vector to the position property
+      // of the text item, to move it in the direction of the
+      // destination point:
+      const dividedVector = vector.divide(30);
+      text.position = text.position.add(dividedVector);
+
+      // If the distance between the path and the destination is less
+      // than 5, we define a new random point in the view to move the
+      // path to:
+      if (vector.length < 5) {
+        destination = Point.random().multiply(paper.view.size);
+      }
     };
-    paper.view.onResize = () => {
-      // const newRandomIndex = getRandomInt(0, this.state.wordlist.length);
-      // text.content = this.state.wordlist[newRandomIndex];
-      text.point.x = paper.view.size.width / 2;
-      text.point.y = paper.view.size.height / 2;
-    };
+    // paper.view.onResize = () => {
+    //   text.point.x = paper.view.size.width / 2;
+    //   text.point.y = paper.view.size.height / 2;
+    // };
     return (
       <div>
         <Canvas />
